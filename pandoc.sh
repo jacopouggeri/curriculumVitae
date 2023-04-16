44d0a53c67530893be3c2c4cf4bd8620b27a8d2e
jacopouggeri <jacopo.uggeri@gmail.com>
2023-04-16 16:15:58 +0200

    Now can compile from local editor

--- pandoc.sh
+++
@@ -1,4 +0,0 @@
-sed 's/centerline/underline/g' Curriculum_Vitae.tex > Curriculum_Vitae_temp.tex
-pandoc Curriculum_Vitae_temp.tex -o Curriculum_Vitae.html
-pandoc publications.tex -o publications.html --bibliography=publications.bib --citeproc --csl=publications.csl
-rm Curriculum_Vitae_temp.tex
