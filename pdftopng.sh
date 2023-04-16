ce5f6ecbf4b7181255bfafa604898f53ca6752c2
jacopouggeri <jacopo.uggeri@gmail.com>
2023-04-16 19:33:17 +0200

    Modified files

--- pdftopng.sh
+++ pdftopng.sh
@@ -4,9 +4,6 @@ mkdir -p png/  # create the png/ directory if it does not already exist
 
 for pdfile in curriculum_vitae.pdf ; do
     convert -verbose -density 500 -background white -alpha remove -alpha off "${pdfile}" "png/${pdfile%.*}_${counter}.png"
-    if ! grep -q "${pdfile%.*}_${counter}.png" README.md; then
-        echo "- Added ${pdfile%.*}_${counter}.png"
-    fi
     counter=$((counter+1))  # increment the counter variable
 done
 
@@ -15,7 +12,9 @@ png_files=(png/curriculum_vitae-*.png)
 
 # generate Markdown text for each PNG file
 for ((i=0; i<${#png_files[@]}; i++)); do
-    echo "![Page $((i+1))](${png_files[$i]})" >> README.md
+    if ! grep -q "${pdfile%.*}_${counter}.png" README.md; then
+        echo "![Page $((i+1))](${png_files[$i]})" >> README.md
+    fi
 done
 
 git add png
