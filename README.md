a81552fa60d3fd30e55893bbdfdf001933d9f144
jacopouggeri <jacopo.uggeri@gmail.com>
2023-04-16 20:18:22 +0200

    Modified files

--- README.md
+++ README.md
@@ -2,12 +2,7 @@
 
 This is the latex project for my CV.
 
-![Page 1](png/curriculum_vitae-0.png)
-![Page 2](png/curriculum_vitae-1.png)
-![Page 3](png/curriculum_vitae-2.png)
-![Page 4](png/curriculum_vitae-3.png)
-![Page 5](png/curriculum_vitae-4.png)
-![Page 6](png/curriculum_vitae-5.png)!
+
 ![Page 1](png/curriculum_vitae-0.png)
 ![Page 2](png/curriculum_vitae-1.png)
 ![Page 3](png/curriculum_vitae-2.png)
--- pdftopng.sh
+++ pdftopng.sh
@@ -1,21 +1,37 @@
 # Generate png images to display in README.md
 
 mkdir -p png/  # create the png/ directory if it does not already exist
-
 for pdfile in curriculum_vitae.pdf ; do
-    convert -verbose -density 500 -background white -alpha remove -alpha off "${pdfile}" "png/${pdfile%.*}_${counter}.png"
-    counter=$((counter+1))  # increment the counter variable
+    echo "Converting ${pdfile}"
+    convert -density 500 -background white -alpha remove -alpha off "${pdfile}" "png/${pdfile%.*}.png"
 done
 
 # create an array of PNG filenames
 png_files=(png/curriculum_vitae-*.png)
 
-# generate Markdown text for each PNG file
+# check which pages are missing and add them to a list
+missing_pages=()
 for ((i=0; i<${#png_files[@]}; i++)); do
-    if ! grep -q "${pdfile%.*}_${counter}.png" README.md; then
-        echo "![Page $((i+1))](${png_files[$i]})" >> README.md
+    page_number=$((i+1))
+    if ! grep -q "^\!\[Page $page_number\]" README.md; then
+        missing_pages+=($page_number)
     fi
 done
+
+# generate Markdown text for the missing pages and append it to the README.md file
+if [ ${#missing_pages[@]} -gt 0 ]; then
+    echo "The following pages are missing:"
+    for page_number in ${missing_pages[@]}; do
+        echo "- Page $page_number"
+    done
+    for ((i=0; i<${#png_files[@]}; i++)); do
+        page_number=$((i+1))
+        if [[ " ${missing_pages[@]} " =~ " $page_number " ]]; then
+            echo "Adding Page $page_number"
+            echo "![Page $page_number](${png_files[$i]})" >> README.md
+        fi
+    done
+fi
 
 git add png
 git add README.md
